13b87050e882bb180d90e75609ce58415ca6ecb3
Havilah J Farnsworth <havilahfarnsworth@gmail.com>
2019-11-12 07:39:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -4,6 +4,7 @@ require_relative './directors_database'
 def directors_totals(source)
   result = {}
   director_index = 0
+  
   while director_index < source.size do
     director = source[director_index]
     result[director[:name]] = gross_for_director(director)
